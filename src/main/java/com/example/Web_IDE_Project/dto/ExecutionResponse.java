c9acb6460629941c9e0e70b7ae5718626f9bac13
BaeYoonSang <yoonsang9802@naver.com>
2026-04-13 02:16:17 +0900

    feat : 코드 결과 DTO

---
+++ src/main/java/com/example/Web_IDE_Project/dto/ExecutionResponse.java
@@ -0,0 +1,13 @@
+package com.example.Web_IDE_Project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExecutionResponse {
+    private String output;
+}
