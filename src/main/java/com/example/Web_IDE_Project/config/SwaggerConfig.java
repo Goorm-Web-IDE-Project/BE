71046bf2c870d5b64e41ed661f0254994d912fd5
BaeYoonSang <yoonsang9802@naver.com>
2026-04-13 01:15:49 +0900

    feat : Swagger 설정

---
+++ src/main/java/com/example/Web_IDE_Project/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.example.Web_IDE_Project.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String securityJwtName = "JWT_Auth"; // 인증 설정 이름
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components().addSecuritySchemes(securityJwtName, new SecurityScheme()
+                .name(securityJwtName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cloud-Link Web IDE API 명세서")
+                        .description("JWT 인증이 포함된 API 문서입니다.")
+                        .version("v1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
