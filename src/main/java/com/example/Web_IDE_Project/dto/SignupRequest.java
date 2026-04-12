c7a87e656f6270f709a73cbc54e1306f997f7ab7
BaeYoonSang <yoonsang9802@naver.com>
2026-04-13 01:14:28 +0900

    feat : 로그인 및 회원가입 필드명 설정

---
+++ src/main/java/com/example/Web_IDE_Project/dto/SignupRequest.java
@@ -0,0 +1,12 @@
+package com.example.Web_IDE_Project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupRequest {
+    private String userId;
+    private String password;
+    private String name;
+}
