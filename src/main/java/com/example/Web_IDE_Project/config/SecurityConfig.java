202f992b0df3bb6a7f0200e792ca2bd55073fe98
BaeYoonSang <yoonsang9802@naver.com>
2026-04-14 09:59:03 +0900

    refactor : 주소 변경

--- src/main/java/com/example/Web_IDE_Project/config/SecurityConfig.java
+++ src/main/java/com/example/Web_IDE_Project/config/SecurityConfig.java
@@ -45,11 +45,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://web-ide-sigma.vercel.app",
-                "https://web-6fdw1biwo-bys0212s-projects.vercel.app"
-        ));
+
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
